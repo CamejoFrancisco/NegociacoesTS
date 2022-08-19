849fc82aaaf68c81e2697c52c6fe903b2aae4d36
Francisco <francisco.camejo.pb@compasso.com.br>
2022-08-19 18:17:11 -0300

    Adicionando métodos e Enumeration

---
+++ app/enums/dias-da-semana.ts
@@ -0,0 +1,10 @@
+//Enums allow a developer to define a set of named constants
+export enum DiasDaSemana {
+    DOMINGO,
+    SEGUNDA,
+    TERCA,
+    QUARTA,
+    QUINTA,
+    SEXTA,
+    SABADO
+}
